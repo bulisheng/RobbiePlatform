cd7a1e38ec4b27f797b5a6ea4489a9890045053a
卜立胜 <457183563@qq.com>
2021-04-21 19:21:16 +0800

    修改

---
+++ RobbiePlatform/Assets/Scripts/2DSample/AudioManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    static AudioManager current;
+    [Header("环境声音")]
+    public AudioClip ammbientClip;
+    public AudioClip musicClip;
+
+    [Header("Robbie 音效")]
+    public AudioClip[] walkStepClips;
+    public AudioClip[] crounchStepClips;
+    public AudioClip jumpClip;
+
+    public AudioClip jumpViodeClip;
+
+    AudioSource ambientSource;
+    AudioSource musicSource;
+    AudioSource fxSource;
+    AudioSource playerSource;
+    AudioSource voiceSource;
+    private void Awake()
+    {
+        current = this;
+        DontDestroyOnLoad(gameObject);
+
+        ambientSource = gameObject.AddComponent<AudioSource>();
+        musicSource = gameObject.AddComponent<AudioSource>();
+        fxSource = gameObject.AddComponent<AudioSource>();
+        playerSource = gameObject.AddComponent<AudioSource>();
+        voiceSource = gameObject.AddComponent<AudioSource>();
+        StartLevelAudio();
+    }
+    void StartLevelAudio()
+    {
+        current.ambientSource.clip = current.ammbientClip;
+        current.ambientSource.loop = true;
+        current.ambientSource.Play();
+
+        current.musicSource.clip = current.musicClip;
+        current.musicSource.loop = true;
+        current.musicSource.Play();
+    }
+    public static void PlayFootStepAudio()
+    {
+        int index = Random.Range(0, current.walkStepClips.Length);
+        current.playerSource.clip = current.walkStepClips[index];
+        current.playerSource.Play();
+    }
+    public static void PlayCrouchStepAudio()
+    {
+        int index = Random.Range(0, current.crounchStepClips.Length);
+        current.playerSource.clip = current.crounchStepClips[index];
+        current.playerSource.Play();
+    }
+    public static void PlayJumpAudio()
+    {
+        current.playerSource.clip = current.jumpClip;
+        current.playerSource.Play();
+
+        current.voiceSource.clip = current.jumpViodeClip;
+        current.voiceSource.Play();
+    }
+}
+
---
+++ RobbiePlatform/Assets/Scripts/2DSample/PlayerAnimator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    PlayerMove playerMove;
+    Rigidbody2D rb;
+    int groundID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerMove = GetComponentInParent<PlayerMove>();
+        rb = GetComponentInParent<Rigidbody2D>();
+        groundID = Animator.StringToHash("isOnGround");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("speed", Mathf.Abs(playerMove.xVelocity));
+        animator.SetBool("isCrouching", playerMove.isCrounch);
+        animator.SetBool("isHanging", playerMove.isHanging);
+        animator.SetBool("isJumping", playerMove.isJump);
+        //用编号传动画状态
+        //animator.SetBool("isOnGround", playerMove.isOnGround);
+        animator.SetBool(groundID, playerMove.isOnGround);
+
+        animator.SetFloat("verticalVelocity", rb.velocity.y);
+    }
+    public void StepAudio()
+    {
+        AudioManager.PlayFootStepAudio();
+    }
+    public void CrouchStepAudio()
+    {
+        AudioManager.PlayCrouchStepAudio();
+    }
+
+}
