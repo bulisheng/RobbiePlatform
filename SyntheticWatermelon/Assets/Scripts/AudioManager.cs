b8e1ef4d0662e927e987235da0fb6c02f9e76d18
卜立胜 <457183563@qq.com>
2021-04-27 14:07:01 +0800

    部分改名

---
+++ SyntheticWatermelon/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    // 获取声音播放组件
+    private AudioSource audioSource;
+
+    public AudioClip[] AudioClips;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// 播放音频
+    /// </summary>
+    /// <param name="audioClip"></param>
+    public void PlayAudio(AudioClip audioClip)
+    {
+        audioSource.PlayOneShot(audioClip);
+    }
+}
---
+++ SyntheticWatermelon/Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    // 分数UI文本组件
+    private Text scoreText;
+    private int score;
+
+    public int Score { get => score;
+        set {
+            score = value;
+            UpdateScoreText(score);
+        }
+    }
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        scoreText = transform.Find("ScoreText").GetComponent<Text>();
+        Score = 0;
+    }
+    /// <summary>
+    /// 更新分数文本
+    /// </summary>
+    void UpdateScoreText(int score )
+    {
+        scoreText.text = score.ToString();
+    }
+}
