adf377b678b87272f8be29aaa8f480a6c2ddc821
卜立胜 <457183563@qq.com>
2021-05-08 20:56:40 +0800

    rgp游戏增加nav寻路

---
+++ 3DRPG/Assets/Scripts/MonseManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
+
+public class MonseManager : MonoBehaviour
+{
+    RaycastHit hitInfo;
+    public EventVector3 onMouseCliscked;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetCursorTexture();
+    }
+    void SetCursorTexture()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            MouseControl();
+        }
+    }
+    void MouseControl()
+    {
+        if (Input.GetMouseButton(0) && hitInfo.collider != null)
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Ground"))
+                onMouseCliscked?.Invoke(hitInfo.point);
+        }
+    }
+}
