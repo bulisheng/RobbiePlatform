84861e75a153336a3dec90524cef7e8c50d32604
卜立胜 <457183563@qq.com>
2021-07-02 14:18:40 +0800

    新增一些发布

---
+++ RobbiePlatform/Assets/Scripts/Login.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using  UnityEngine.UI;
+public class Login : MonoBehaviour
+{
+    public InputField m_ID;
+
+    public InputField m_Password;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnButton()
+    {
+        if (m_ID.text=="111"&&m_Password.text=="chi")
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
